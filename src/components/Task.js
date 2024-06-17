ec98de79f06829d94219b4337681ed8da37bc24f
VitorHugoNP <supimpayey@gmail.com>
2024-06-17 08:41:32 -0300

    mudaças no Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,22 @@
 import react from "react"
-import {Text, View, StyleSheet} from "react-native"
+import {Text, View, StyleSheet, TouchableWithoutFeedback} from "react-native"
 import moment from "moment"
+import { Icon } from "react-native-vector-icons/FontAwesome6"
 
 export default props => {
+    <TouchableWithoutFeedback>
+        <view style={styles.checkContainer}>
+            <view style={styles.done}>
+                <icon name="check" size={20} color="#FFF"></icon>
+            </view>
+        </view>
+    </TouchableWithoutFeedback>
     return(
         <View style={styles.container}>
             <View>
-                <Text>{props.description}</Text>
-                <Text>{props.estimate_at}</Text>
-                <Text>{props.done_at}</Text>
+                <Text styles={styles.desc}>{props.description}</Text>
+                <Text styles={styles.date}>{props.estimate_at + ""}</Text>
+                <Text styles={styles.date}>{props.done_at + ""}</Text>
             </View>
         </View>
     )
@@ -20,5 +28,35 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         alignItems: 'center',
         paddingVertical: 10
+    },
+    checkContainer:{
+        width: "20%",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    done:{
+        height: 25,
+        width: 20,
+        borderRadius: 13,
+        borderWidth: 1,
+        backgroundColor: "#4D7031",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    pending:{
+        height: 25,
+        width: 20,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: "#555"
+    },
+    desc:{
+        fontFamily: "Arial",
+        fontSize: 20,
+        color: "#333"
+    },
+    date:{
+        fontFamily: "Arial",
+        color: "#333"
     }
 })
